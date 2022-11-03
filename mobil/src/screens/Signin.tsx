5b4cd6819be2cd651d51cf3d1f46f82a76115a6d
Luis Felipe Ribeiro <luis@MacBook-Air-de-Luis.local>
2022-11-03 11:18:53 -0300

    <feat> Geral: Estrututa Inicial das aplicações server, web e mobile

---
+++ mobil/src/screens/Signin.tsx
@@ -0,0 +1,11 @@
+import { NativeBaseProvider, Text, Center } from "native-base";
+
+export function Signin() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text color="white" fontSize={24}>
+        Hello NLW
+      </Text>
+    </Center>
+  );
+}
